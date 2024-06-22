b74772d4fc18c3ff5c0212e2e311a699985ff474
Aryan Chand <aryanchand753@gmail.com>
2024-06-21 21:19:21 -0400

    updated functions to handle the `hidden` status

--- admin/competitions/CUD.php
+++ admin/competitions/CUD.php
@@ -1,65 +1,82 @@
 <?php
 
-function createCompetition($comp_name, $start_date, $end_date, $payment_id, $show_forms, $show_bus, $show_room, $comp_desc): bool
+function createCompetition($comp_name, $start_date, $end_date, $payment_id, $show_forms, $show_bus, $show_room, $comp_desc, $hidden): bool
 {
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/sql.php";
-	$sql_conn = getDBConn();
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/sql.php";
+    $sql_conn = getDBConn();
 
-	$create_competition_stmt = $sql_conn->prepare(
-		"INSERT INTO competitions (competition_name, start_date, end_date, payment_id, show_forms, show_bus, show_room, description)
-			   VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $create_competition_stmt = $sql_conn->prepare(
+        "INSERT INTO competitions (competition_name, start_date, end_date, payment_id, show_forms, show_bus, show_room, description, hidden)
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    );
 
-	if (empty($payment_id))
-		$payment_id = null;
-	$create_competition_stmt->bind_param('ssssiiis',
-		$comp_name, $start_date, $end_date, $payment_id,
-		$show_forms, $show_bus, $show_room,
-		$comp_desc);
+    if (empty($payment_id))
+        $payment_id = null;
+    $create_competition_stmt->bind_param('ssssiiisi',
+        $comp_name, $start_date, $end_date, $payment_id,
+        $show_forms, $show_bus, $show_room,
+        $comp_desc, $hidden
+    );
 
-	return $create_competition_stmt->execute() && $sql_conn->close();
+    return $create_competition_stmt->execute() && $sql_conn->close();
 }
 
-function updateCompetition($comp_name, $start_date, $end_date, $payment_id, $show_forms, $show_bus, $show_room, $comp_desc): bool
+function updateCompetition($comp_name, $start_date, $end_date, $payment_id, $show_forms, $show_bus, $show_room, $comp_desc, $hidden): bool
 {
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/sql.php";
-	$sql_conn = getDBConn();
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/sql.php";
+    $sql_conn = getDBConn();
 
-	$update_competition_stmt = $sql_conn->prepare(
-		"UPDATE competitions SET start_date = ?, end_date = ?, payment_id = ?, show_forms = ?, show_bus = ?, show_room = ?, description = ?
-			   WHERE competition_name = ?");
+    $update_competition_stmt = $sql_conn->prepare(
+        "UPDATE competitions SET start_date = ?, end_date = ?, payment_id = ?, show_forms = ?, show_bus = ?, show_room = ?, description = ?, hidden = ?
+               WHERE competition_name = ?"
+    );
 
-	if (empty($payment_id))
-		$payment_id = null;
-	$update_competition_stmt->bind_param('sssiiiss',
-		$start_date, $end_date, $payment_id,
-		$show_forms, $show_bus, $show_room,
-		$comp_desc,
-		$comp_name);
+    if (empty($payment_id))
+        $payment_id = null;
+    $update_competition_stmt->bind_param('sssiiissi',
+        $start_date, $end_date, $payment_id,
+        $show_forms, $show_bus, $show_room,
+        $comp_desc, $hidden,
+        $comp_name
+    );
 
-	return $update_competition_stmt->execute() && $sql_conn->close();
+    return $update_competition_stmt->execute() && $sql_conn->close();
 }
 
-// Deletes corresponding competitions and transactions
+function hideCompetition($comp_name): bool
+{
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/sql.php";
+    $sql_conn = getDBConn();
+
+    // Update hidden attribute
+    $hide_comp_stmt = $sql_conn->prepare("UPDATE competitions SET hidden = 1 WHERE competition_name = ?");
+    $hide_comp_stmt->bind_param('s', $comp_name);
+    $result_comp = $hide_comp_stmt->execute();
+
+    $sql_conn->close();
+    return $result_comp;
+}
+
 function deleteCompetition($comp_name): bool
 {
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/sql.php";
-	$sql_conn = getDBConn();
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/sql.php";
+    $sql_conn = getDBConn();
 
-	// Competition
-	$delete_comp_stmt = $sql_conn->prepare("DELETE FROM competitions WHERE competition_name = ?");
-	$delete_comp_stmt->bind_param('s', $comp_name);
-	$result_comp = $delete_comp_stmt->execute();
+    // Competition
+    $delete_comp_stmt = $sql_conn->prepare("DELETE FROM competitions WHERE competition_name = ?");
+    $delete_comp_stmt->bind_param('s', $comp_name);
+    $result_comp = $delete_comp_stmt->execute();
 
-	// Competition Data
-	$delete_comp_data_stmt = $sql_conn->prepare("DELETE FROM competition_data WHERE competition_name = ?");
-	$delete_comp_data_stmt->bind_param('s', $comp_name);
-	$result_comp_data = $delete_comp_data_stmt->execute();
+    // Competition Data
+    $delete_comp_data_stmt = $sql_conn->prepare("DELETE FROM competition_data WHERE competition_name = ?");
+    $delete_comp_data_stmt->bind_param('s', $comp_name);
+    $result_comp_data = $delete_comp_data_stmt->execute();
 
-	// Competition Selections
-	$delete_comp_selections_stmt = $sql_conn->prepare("DELETE FROM competition_selections WHERE competition_name = ?");
-	$delete_comp_selections_stmt->bind_param('s', $comp_name);
-	$result_comp_selections = $delete_comp_selections_stmt->execute();
+    // Competition Selections
+    $delete_comp_selections_stmt = $sql_conn->prepare("DELETE FROM competition_selections WHERE competition_name = ?");
+    $delete_comp_selections_stmt->bind_param('s', $comp_name);
+    $result_comp_selections = $delete_comp_selections_stmt->execute();
 
-	$sql_conn->close();
-	return ($result_comp && $result_comp_data && $result_comp_selections);
+    $sql_conn->close();
+    return ($result_comp && $result_comp_data && $result_comp_selections);
 }
--- admin/competitions/manage.php
+++ admin/competitions/manage.php
@@ -10,36 +10,57 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/permissions.php";
 checkPerms(OFFICER_PERMS);
 
 if (isset($_POST['create'])) {
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/competitions/CUD.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/competitions/CUD.php";
 
-	$created = createCompetition(  // Create competition
-		$_POST['comp_name'], $_POST['start_date'], $_POST['end_date'], $_POST['payment_id'],
-		isset($_POST['forms']), isset($_POST['bus']), isset($_POST['room']),
-		$_POST['info']);
+    $hidden = isset($_POST['hidden']) ? 1 : 0;
 
-	redirect(currentURL(false) . '?comp_name=' . rawurlencode($_POST['comp_name'])); // Redirect to created competition manage page
+    $created = createCompetition(
+        $_POST['comp_name'], $_POST['start_date'], $_POST['end_date'], $_POST['payment_id'],
+        isset($_POST['forms']), isset($_POST['bus']), isset($_POST['room']),
+        $_POST['info'], $hidden
+    );
+
+    redirect(currentURL(false) . '?comp_name=' . rawurlencode($_POST['comp_name']));
 } else if (isset($_POST['update'])) {
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/competitions/CUD.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/competitions/CUD.php";
 
-	updateCompetition(  // Update competition
-		$_POST['comp_name'], $_POST['start_date'], $_POST['end_date'], $_POST['payment_id'],
-		isset($_POST['forms']), isset($_POST['bus']), isset($_POST['room']),
-		$_POST['info']);
+    $hidden = isset($_POST['hidden']) ? 1 : 0;
 
-	redirect(currentURL());
+    updateCompetition(
+        $_POST['comp_name'], $_POST['start_date'], $_POST['end_date'], $_POST['payment_id'],
+        isset($_POST['forms']), isset($_POST['bus']), isset($_POST['room']),
+        $_POST['info'], $hidden
+    );
+
+    redirect(currentURL());
 } else if (isset($_POST['delete'])) {
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/competitions/CUD.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/competitions/CUD.php";
 
-	deleteCompetition($_POST['comp_name']); // Delete competition
+    if (isset($_POST['hide'])) {
+        hideCompetition($_POST['comp_name']);
+    } else {
+        deleteCompetition($_POST['comp_name']);
+    }
 
-	redirect(currentURL(false));    // Redirect to deselected manage page
+    redirect(currentURL(false));
 }
 
 require_once $_SERVER['DOCUMENT_ROOT'] . "/shared/competitions.php";
 
 $comp = null;
 if (isset($_GET['comp_name']) && existsComp($_GET['comp_name']))
-	$comp = $_GET['comp_name']
+    $comp = $_GET['comp_name'];
+
+$start_date = getAssociatedCompInfo($comp, 'start_date');
+$end_date = getAssociatedCompInfo($comp, 'end_date');
+$payment_id = getAssociatedCompInfo($comp, 'payment_id');
+
+$check_status_forms = (getAssociatedCompInfo($comp, 'show_forms') ?? true ? 'checked' : '');
+$check_status_bus = (getAssociatedCompInfo($comp, 'show_bus') ?? true ? 'checked' : '');
+$check_status_room = (getAssociatedCompInfo($comp, 'show_room') ?? true ? 'checked' : '');
+$check_status_hidden = (getAssociatedCompInfo($comp, 'hidden') ? 'checked' : '');
+
+$description = getDetail('competitions', 'description', 'competition_name', $comp);
 ?>
 
 <title>MAO | Competitions</title>
@@ -50,42 +71,26 @@ if (isset($_GET['comp_name']) && existsComp($_GET['comp_name']))
     <fieldset>
         <legend><b>Competition</b></legend>
 
-        <!--suppress HtmlFormInputWithoutLabel -->
         <select name="comp_name" onchange="this.form.submit()" style="margin-bottom: 6px;">
             <option selected disabled hidden></option>
-			<?php
-			$sql_conn = getDBConn();
+            <?php
+            $sql_conn = getDBConn();
 
-			$comp_names_stmt = $sql_conn->prepare("SELECT competition_name FROM competitions");
-			$comp_names_stmt->bind_result($curr_comp_name);
-			$comp_names_stmt->execute();
+            $comp_names_stmt = $sql_conn->prepare("SELECT competition_name FROM competitions WHERE NOT hidden");
+            $comp_names_stmt->bind_result($curr_comp_name);
+            $comp_names_stmt->execute();
 
-			while ($comp_names_stmt->fetch()) {
-				if ($curr_comp_name == $comp)
-					echo "<option value='$curr_comp_name' selected>$curr_comp_name</option>";
-				else
-					echo "<option value='$curr_comp_name'>$curr_comp_name</option>";
-			}
-			?>
+            while ($comp_names_stmt->fetch()) {
+                if ($curr_comp_name == $comp)
+                    echo "<option value='$curr_comp_name' selected>$curr_comp_name</option>";
+                else
+                    echo "<option value='$curr_comp_name'>$curr_comp_name</option>";
+            }
+            $sql_conn->close();
+            ?>
         </select>
-        <br>
-
-        <button onclick="location.href='<?php echo currentURL(false); ?>'" type="button">Deselect</button>
     </fieldset>
 </form>
-<br>
-
-<?php
-$start_date = getAssociatedCompInfo($comp, 'start_date');
-$end_date = getAssociatedCompInfo($comp, 'end_date');
-$payment_id = getAssociatedCompInfo($comp, 'payment_id');
-
-$check_status_forms = (getAssociatedCompInfo($comp, 'show_forms') ?? true ? 'checked' : 'unchecked');
-$check_status_bus = (getAssociatedCompInfo($comp, 'show_bus') ?? true ? 'checked' : 'unchecked');
-$check_status_room = (getAssociatedCompInfo($comp, 'show_room') ?? true ? 'checked' : 'unchecked');
-
-$description = getDetail('competitions', 'description', 'competition_name', $comp);
-?>
 
 <form method="post" class="filled border" style="text-align: center;">
     <fieldset>
@@ -95,11 +100,11 @@ $description = getDetail('competitions', 'description', 'competition_name', $com
             <label for="comp_name">Competition:</label>
             <input id="comp_name" name="comp_name" type="text" required
                    value="<?php echo $comp; ?>"
-				<?php if (!is_null($comp)) echo 'disabled'; ?>>
-			<?php
-			if (!is_null($comp))
-				echo "<input name='comp_name' type='hidden' value='$comp'>";
-			?><br>
+                <?php if (!is_null($comp)) echo 'disabled'; ?>>
+            <?php
+            if (!is_null($comp))
+                echo "<input name='comp_name' type='hidden' value='$comp'>";
+            ?><br>
 
             <label for="start_date">Start Date:</label>
             <input id="start_date" name="start_date" type="date"
@@ -112,20 +117,20 @@ $description = getDetail('competitions', 'description', 'competition_name', $com
             <label for="payment_id">Payment ID:</label>
             <select id="payment_id" name="payment_id">
                 <option selected></option>
-				<?php
-				$payments_stmt = $sql_conn->prepare("SELECT payment_id FROM payment_details");
-				$payments_stmt->bind_result($curr_payment_id);
-				$payments_stmt->execute();
+                <?php
+                $payments_stmt = $sql_conn->prepare("SELECT payment_id FROM payment_details");
+                $payments_stmt->bind_result($curr_payment_id);
+                $payments_stmt->execute();
 
-				while ($payments_stmt->fetch()) {
-					if ($curr_payment_id == $payment_id)
-						echo "<option value='$curr_payment_id' selected>$curr_payment_id</option>";
-					else
-						echo "<option value='$curr_payment_id'>$curr_payment_id</option>";
-				}
+                while ($payments_stmt->fetch()) {
+                    if ($curr_payment_id == $payment_id)
+                        echo "<option value='$curr_payment_id' selected>$curr_payment_id</option>";
+                    else
+                        echo "<option value='$curr_payment_id'>$curr_payment_id</option>";
+                }
 
-				$sql_conn->close();
-				?>
+                $sql_conn->close();
+                ?>
             </select>
         </div>
         <br>
@@ -141,17 +146,21 @@ $description = getDetail('competitions', 'description', 'competition_name', $com
 
             <input id="room" name="room" type="checkbox" <?php echo $check_status_room ?>>
             <label for="room">Room #</label><br>
+
+            <input id="hidden" name="hidden" type="checkbox" <?php echo $check_status_hidden ?>>
+            <label for="hidden">Hidden</label><br>
         </div>
         <br>
 
         <h4><u>Description</u></h4>
-        <!--suppress HtmlFormInputWithoutLabel -->
         <textarea id="info" name="info" rows="10" cols="50"><?php echo $description; ?></textarea><br>
         <br>
         <input id="create" name="create" type="submit" value="Create"
                style="color: green; float: left;" <?php if (!is_null($comp)) echo 'disabled'; ?>>
         <input id="update" name="update" type="submit" value="Update"
                style="color: blue;" <?php if (is_null($comp)) echo 'disabled'; ?>>
+        <input id="hide" name="hide" type="submit" value="Hide"
+               style="color: orange;" <?php if (is_null($comp)) echo 'disabled'; ?>>
         <input id="delete" name="delete" type="submit" value="Delete"
                style="color: red; float: right;" <?php if (is_null($comp)) echo 'disabled'; ?>>
     </fieldset>
